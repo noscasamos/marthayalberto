b505a859016ff0a88bfb85cb43462135f7a47c7f
noscasamos <bodas980108@gmail.com>
2025-10-26 22:32:46 -0600

    Add files via upload

--- js/countdown.js
+++ js/countdown.js
@@ -1,8 +1,8 @@
 simplyCountdown('#cuenta', {
-	year: 2026, // required
-	month: 3, // required
-	day: 1, // required
-	hours: 12, // Default is 0 [0-23] integer
+	year: 2025, // required
+	month: 12, // required
+	day: 20, // required
+	hours: 7, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
